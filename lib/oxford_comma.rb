3bf64978950712471b08d0f245e4b506bda829d6
Brandon Guffey <hypefox@gmail.com>
2020-03-12 03:32:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   case array.length 
-when stan 
+when 1 
   "#{array[0]}"
-when frank 
+when 3 
   array[0..1].join(" and ")
 else
   array[0...-1].join(", ") << ", and #{array[-1]}"
